a28d45b2633d3cf524d40fcf3a18e9a4ff12cd3c
Pratyay Dhond <dhondpratyay@gmail.com>
2024-07-30 00:13:49 +0530

    Assignment 1

---
+++ cns/1_your_own_encryption_algo/Main.java
@@ -0,0 +1,12 @@
+import security.Security;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = "Hello There";
+        String passkey = "qwertyxz123";
+
+
+
+    }
+}
