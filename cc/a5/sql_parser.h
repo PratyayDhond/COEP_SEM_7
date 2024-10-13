9f3343bd80117bcba5e475ed0728b5bf981d82d0
Pratyay Dhond <dhondpratyay@gmail.com>
2024-10-13 19:13:27 +0530

    Removed Comments

--- cc/a5/sql_parser.h
+++ cc/a5/sql_parser.h
@@ -3,13 +3,8 @@
 
 #include <stdbool.h>
 
-// Function to validate SQL query syntax
 bool validate_sql_query(const char *query);
-
-// Function to extract SQL keywords (like SELECT, INSERT, etc.)
 void extract_sql_keywords(const char *query);
-
-// Function to print syntax errors if any
 void print_sql_error(const char *query);
 
 #endif
